e02e65536bbf05c0dbc0111cd9993212c02b9c2f
v-nvtsk <v@nvtsk.ru>
2023-09-05 23:53:03 +0300

    Add js scripts for phone-input

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const phoneInput = document.querySelector("#phone");
+  const iti = window.intlTelInput(phoneInput, {
+    customContainer: 'get-vaccine__phone-input-container',
+    initialCountry: "auto",
+    utilsScript: '../lib/phone/utils.js',
+    nationalMode: 'false',
+    autoInsertDialCode: 'true',
+    allowDropdown: 'true',
+    // separateDialCode: 'true',
+    geoIpLookup: function (callback) {
+      fetch("https://ipapi.co/json")
+        .then(function (res) { return res.json(); })
+        .then(function (data) { callback(data.country_code); })
+        .catch(function () { callback("ru"); });
+    }
+  });
+
+
+  const verifyButton = document.querySelector('.get-vaccine__verify-btn');
+
+  verifyButton.addEventListener('click', (ev) => {
+    if (!iti.isPossibleNumber()) {
+      phoneInput.classList.add('invalid');
+    }
+  })
+  phoneInput.addEventListener('input', (ev) => {
+    phoneInput.classList.remove('invalid');
+  })
+})
